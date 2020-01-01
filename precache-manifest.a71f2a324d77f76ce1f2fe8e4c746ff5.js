6a4cd9c6b5943ae2019365d49312dc623f8e0d7f
Priya <guptapriyab@gmail.com>
2020-01-01 23:43:20 +0100

    Updates

---
+++ precache-manifest.a71f2a324d77f76ce1f2fe8e4c746ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37db0b44b3438bf8430a29d9e3e3e89e",
+    "url": "/single-page-profile/index.html"
+  },
+  {
+    "revision": "9c80ff4405edef691658",
+    "url": "/single-page-profile/static/css/main.8d92071b.chunk.css"
+  },
+  {
+    "revision": "e830d4f3dd0eb6c9ca61",
+    "url": "/single-page-profile/static/js/2.0421c2d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/single-page-profile/static/js/2.0421c2d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c80ff4405edef691658",
+    "url": "/single-page-profile/static/js/main.2db8169f.chunk.js"
+  },
+  {
+    "revision": "b68a8ec8393cc369a516",
+    "url": "/single-page-profile/static/js/runtime-main.5a1eab08.js"
+  }
+]);
